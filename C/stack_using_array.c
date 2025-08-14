0354479c9e2af2ac98b95da78e6ceca3339e694e
estoyj001 <ajunaidkb14@gmail.com>
2025-08-14 15:31:45 +0530

    aug 14

---
+++ C/stack_using_array.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+int ch,a[30];
+
+printf("Enter Operation\n 1.Push  2.Pop  3.Display  4.Exit");
+scanf("%d", ch);
+
+switch(ch){
+    case 1: printf("Enter value to push");
+            scanf("%d")
+
+}
+
+}
