67eb8eb5ee2d81e1102320485dac1f9a7645abb3
Rana-lk <spongebabuu@gmail.com>
2025-09-24 11:59:05 +0530

    sep24

---
+++ python/list_check.py
@@ -0,0 +1,4 @@
+list1 = list(map(int, input("Enter numbers: ").split()))
+list2 = list(map(int, input("Enter numbers: ").split()))
+
+
---
+++ python/n_mult.py
@@ -0,0 +1,5 @@
+num = input("Enter number: ")
+lim = input("Multiplication upto: ")
+
+for i in range(1,lim+1):
+    print(num*i)
