889566379ce9fc2348bac6cfea8480564011700a
estoyj001 <ajunaidkb14@gmail.com>
2025-10-27 10:44:32 +0530

    oct 27

---
+++ php/form.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Enter Name</title>
+</head>
+<body>
+
+<h2>Enter Your Name</h2>
+
+<form action="welcome.php" method="post">
+    <label>Your Name:</label>
+    <input type="text" name="username" >
+    <input type="submit" value="Submit">
+    <input type="reset" name="" id="" value="Reset">
+</form>
+
+</body>
+</html>
