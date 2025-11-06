68fdbabf0d282a66f9adbf9edacb2fb9d12a6548
estoyj001 <ajunaidkb14@gmail.com>
2025-11-06 10:58:46 +0530

    nov 6

--- php/stud_reg/login.php
+++ php/stud_reg/login.php
@@ -1,31 +1,35 @@
 <?php
-session_start();
+if($_SERVER["REQUEST_METHOD"]=="POST"){
+
 $conn = mysqli_connect("localhost", "root", "", "my_test_db");
 
 if (!$conn) {
     die("Database Error: " . mysqli_connect_error());
 }
 
-$username = $_POST['username'] ?? '';
-$password = $_POST['password'] ?? '';
+
+$username = $_POST['username'] ;
+$password = $_POST['password'] ;
 
 $query = "SELECT * FROM login WHERE username='$username' AND password='$password'";
 $result = mysqli_query($conn, $query);
 
-if (mysqli_num_rows($result) > 0) {
-    $row = mysqli_fetch_assoc($result);
-    $_SESSION['username'] = $row['username'];
-    $_SESSION['usertype'] = $row['usertype'];
+echo $result;
 
-    if ($row['usertype'] == 'admin') {
+if (mysqli_num_rows($result) > 0) {
+
         header("Location: admin_home.html");
-    } else if ($row['usertype'] == 'student') {
-        header("Location: student_home.php");
-    }
+
+    // $row = mysqli_fetch_assoc($result);
+
+    // if ($row['usertype'] == 'admin') {
+    //     header("Location: admin_home.html");
+    // }
 } else {
     echo "<script>
-                alert('Database Error: " . addslashes(mysqli_error($conn)) . "');
+                alert('Incorrect " . addslashes(mysqli_error($conn)) . "');
                 window.location.href = 'login.html';
               </script>";
+}
 }
 ?>
